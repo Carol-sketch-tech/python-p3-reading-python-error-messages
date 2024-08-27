2057f9f301f539073876c4c77597a59b8ac55828
Caroline <bancroftwanjiru10000@gmail.com>
2024-08-27 15:35:13 +0300

    error debuggin passed

---
+++ lib/app.py
@@ -0,0 +1,7 @@
+# types of errors -> suntax errors result from incorrect syntax.
+# N/b NOT THAT EVERY OPERATOR MUST BE FOLLOWED AND PRECEDED BY A NUMBER OR VARIBALE WITH A NUMERICAL VALUE.
+
+# 2 LOGICAL ERROS ->  lOGICAL EORRORS are laway hard to find because they are not percieved by hte python interpreator itself.
+# 3. Exceptions Error ->  cover a variety of errors that you may see when running python code.- they pop up when the interpratot knows what to do with a piece of code 
+# but unable to complete execution 
+# examples of these errors are 1. assertions error, 2. indexError and keyerror 3. nName error -> when a name is refferenced but nor defined. 4. Type Error -> is when am operation or function is applied to an object of the wrong type.
